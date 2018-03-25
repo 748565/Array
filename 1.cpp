436d83d02d7852c2c5a192a0d242fb618432ee9a
748565 <31278546+748565@users.noreply.github.com>
2018-03-25 11:09:12 +0530

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,28 +1,26 @@
+ 
+
 #include <iostream>
 using namespace std;
 
-int main() {
-int n, min;
-cout << "define any size of array";
-cin >> n;
+int main ()
+{
+  int n ;
+  cout<<"Enter the Size of the Array\n";
+  cin>>n;
+  int array[n];
+  cout<<"Enter the elements in the array ";
+  for(int i = 0;i<n;i++)
+    {
+      cin>>array[i];
+    }
 
-int A[n];
-//cout << "A[ << "n" << ]has been created";
+    int smallest = array[0] ;
+    for ( int i=0;  i < n;  i++ )
+        if ( array[i] < smallest )
+             smallest = array[i] ;
 
-int i;
-for(i=0;i<n;i++) {
-cin >> A[i];
-}
-cout << "find the smallest element in array";
-min= A[i];
-for(i=0;i<n;i++){
-  if (A[i]<min)
-      min = A[i];
-  else
-      min =! A[i];
-  }          
-  {
-cout << "smallest element =" << min;
-  }
-return 0;
+    cout << "The smallest number in the array is " <<smallest<< '\n' ;
+
+    return 0;
 }
