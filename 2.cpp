617677d9c04e14fd4773a5550eba0f46573ed540
748565 <31278546+748565@users.noreply.github.com>
2018-03-25 11:30:08 +0530

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,13 +1,10 @@
-
-
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
 
- a[n];
-int i,n,j,s;
-cout<<"Enter the value of n:"<<endl;
+int main(){
+int i,n,j,s;    
+int a[n];
+cout<<"Enter the size of array:"<<endl;
 cin>>n;
 cout<<"\nEnter the values:\n";
 for(i=0;i<n;i++)
@@ -19,8 +16,8 @@ for(i=0;i<n;i++)
 for(j=0;j<n-1;j++)
 {
 if(a[j]>a[j+1])
-
-S=a[j];
+{
+s=a[j];
 a[j]=a[j+1];
 a[j+1]=s;
 }
@@ -29,7 +26,10 @@ a[j+1]=s;
 cout<<"Sorted array:\n";
 for(i=0;i<n;i++)
 {
-cout<<a[i]<<"\n;
+cout<<a[i] << "_";
 }
+return 0;
+}
+    
 
-}
+
